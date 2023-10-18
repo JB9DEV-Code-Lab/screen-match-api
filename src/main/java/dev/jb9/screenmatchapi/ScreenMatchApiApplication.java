a27922940a393fa7ff9d1bf5e2b9c6f2c8663323
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-10-18 14:01:11 +0100

    feat: implement CommandLineRunner on main application method

--- src/main/java/dev/jb9/screenmatchapi/ScreenMatchApiApplication.java
+++ src/main/java/dev/jb9/screenmatchapi/ScreenMatchApiApplication.java
@@ -1,13 +1,19 @@
 package dev.jb9.screenmatchapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class ScreenMatchApiApplication {
+public class ScreenMatchApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenMatchApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Just following the course instructions, to implement a command line runner here");
 	}
 }
