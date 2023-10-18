7b646c6a8a1c47a9b1e71ec75d62c8ac6af108d9
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-10-18 13:58:29 +0100

    build: standard Spring Boot project structure

---
+++ src/main/java/dev/jb9/screenmatchapi/ScreenMatchApiApplication.java
@@ -0,0 +1,13 @@
+package dev.jb9.screenmatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ScreenMatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApiApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/jb9/screenmatchapi/ScreenMatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jb9.screenmatchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
