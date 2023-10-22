3281de63d64014c1d590d51e646fd603d90abeba
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-10-22 23:36:10 +0100

    feat: Episode DTO

---
+++ src/main/java/dev/jb9/screenmatchapi/dtos/EpisodeDTO.java
@@ -0,0 +1,15 @@
+package dev.jb9.screenmatchapi.dtos;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record EpisodeDTO (
+        @JsonAlias("Title")
+        String title,
+        @JsonAlias("Episode")
+        String episodeNumber,
+        @JsonAlias("Released")
+        String releasedAt,
+        String imdbRating
+) {}
